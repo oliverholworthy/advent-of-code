9a8f9e7086b969f24b891c2c4bbccbde5dc4e8d1
Oliver Holworthy <oliver@holworthy.com>
2017-12-06 18:59:42 +0000

    Day 1 - Part Two

--- src/advent_of_code/2017/1.clj
+++ src/advent_of_code/2017/1.clj
@@ -2,22 +2,46 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn captcha [numbers]
-  (let [xs (map #(Long/parseLong (str %)) (str numbers))]
-    (:res (reduce
-           (fn [{:keys [res prev]} x]
-             {:res (if (and prev (= prev x))
-                     (+ res x)
-                     res)
-              :prev x})
-           {:res 0
-            :prev nil}
-           (cons (last xs) xs)))))
 
-(defn test-captcha []
-  (assert (= (captcha 1122) 3))
-  (assert (= (captcha 1111) 4))
-  (assert (= (captcha 1234) 0))
-  (assert (= (captcha 91212129) 9)))
+(defn captcha-circular [steps-fn]
+  (fn [numbers]
+    (let [xs (map #(Long/parseLong (str %)) (str numbers))
+          steps (steps-fn xs)]
+      (:res (reduce
+             (fn [{:keys [res next]} x]
+               {:res (if (= (first next) x)
+                       (+ res x)
+                       res)
+                :next (rest next)})
+             {:res 0
+              :next (drop steps (cycle xs))}
+             xs)))))
 
-(def input (str/trim (slurp (io/resource "2017/1/input.txt"))))
+
+;; Part One
+
+(def captcha-next (captcha-circular (fn [xs] (inc (count xs)))))
+
+(defn test-captcha-next []
+  (let [f captcha-next]
+    (assert (= (f 1122) 3))
+    (assert (= (f 1111) 4))
+    (assert (= (f 1234) 0))
+    (assert (= (f 91212129) 9))))
+
+(def input-part-one (str/trim (slurp (io/resource "2017/1/input-1.txt"))))
+
+
+;; Part Two
+
+(def captcha-halfway (captcha-circular (fn [xs] (/ (count xs) 2))))
+
+(defn test-captcha-halfway []
+  (let [f captcha-halfway]
+    (assert (= (f 1212) 6))
+    (assert (= (f 1221) 0))
+    (assert (= (f 123425) 4))
+    (assert (= (f 123123) 12))
+    (assert (= (f 12131415) 4))))
+
+(def input-part-two (str/trim (slurp (io/resource "2017/1/input-2.txt"))))
