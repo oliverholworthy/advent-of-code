eb55908a3e7a174a7b51409a61a583c8867e6ed1
Oliver Holworthy <oliver@holworthy.com>
2017-12-08 00:07:30 +0000

    Day 4 - Part Two

--- 4.clj
+++ 4.clj
@@ -2,11 +2,23 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn valid? [passphrase]
+;; Part One
+
+(def input-passphrases (str/split-lines (str/trim (slurp (io/resource "2017/4/passphrases.txt")))))
+
+(defn valid-words? [passphrase]
   (let [words (str/split passphrase #" ")]
     (= (count (set words)) (count words))))
 
-(def input-part-one (str/split-lines (str/trim (slurp (io/resource "2017/4/input-1.txt")))))
-
-;; (count (filter valid? input-part-one))
+;; (count (filter valid-words? input-passphrases))
 ;; => 451
+
+
+;; Part Two
+
+(defn valid-anagrams? [passphrase]
+  (let [words (str/split passphrase #" ")]
+    (= (count (set (map set words))) (count (map set words)))))
+
+;; (count (filter valid-anagrams? input-passphrases))
+;; => 223
