24173d7dd8daf1b1bfe7b3b0c94017cbc7a54023
Oliver Holworthy <oliver@holworthy.com>
2017-12-09 16:26:35 +0000

    Day 6

---
+++ src/advent_of_code/2017/6.clj
@@ -0,0 +1,54 @@
+(ns advent-of-code.2017.6
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn max-loc [banks]
+  (reduce
+   (fn [[acc-i acc-bank] [i bank]]
+     (if (> bank acc-bank)
+       [i bank]
+       [acc-i acc-bank]))
+   [0 0]
+   (map-indexed vector banks)))
+
+(defn redistribute [banks]
+  (let [[loc-i loc-bank] (max-loc banks)
+        n (count banks)
+        bank-quot (quot loc-bank n)
+        b (if (zero? bank-quot) loc-bank (rem loc-bank (* n bank-quot)))]
+    (vec (map-indexed (fn [i bank]
+                        (+ (if (= i loc-i) 0 bank)
+                           bank-quot
+                           (let [i2 (mod (+ i (- n loc-i)) n)]
+                             (if (and (<= i2 b) (> i2 0))
+                               1 0))))
+                      banks))))
+
+(defn memory-allocation
+  [banks]
+  (loop [configurations (hash-set banks)
+         banks banks
+         steps 0]
+    (let [new-banks (redistribute banks)]
+      (if (contains? configurations new-banks)
+        [(inc steps) banks]
+        (recur (conj configurations new-banks)
+               new-banks
+               (inc steps))))))
+
+(def input
+  (->> (slurp (io/resource "2017/6/input.txt"))
+       (str/trim )
+       (#(str/split % #"\s+"))
+       (mapv #(Long/parseLong %))))
+
+;; Part One
+
+(defn cycles [banks]
+  (first (memory-allocation banks)))
+
+;; Part Two
+
+(defn loop-size
+  [banks]
+  (first (memory-allocation (second (memory-allocation banks)))))
