722a016776389187bb76f8c0b08870d9887554c0
Oliver Holworthy <oliver@holworthy.com>
2017-12-11 19:52:36 +0000

    Day 11 - convert move-dirs keys to keywords

--- src/advent_of_code/2017/11.clj
+++ src/advent_of_code/2017/11.clj
@@ -2,35 +2,30 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def move-dirs
-  {"ne" [ 1  0 -1]
-   "n"  [ 0  1 -1]
-   "nw" [-1  1  0]
-   "sw" [-1  0  1]
-   "s"  [ 0 -1  1]
-   "se" [ 1 -1  0]})
+(def move-dir {:nw [-1  1  0] :n [0  1 -1] :ne [1  0 -1]
+               :sw [-1  0  1] :s [0 -1  1] :se [1 -1  0]})
 
-(defn move-dir [move] (get move-dirs move))
 (defn manhattan [pos] (/ (reduce + (mapv #(Math/abs %) pos)) 2))
-(defn apply-dirs [dirs] (apply mapv + dirs))
-(defn apply-dir [pos dir] (mapv + pos dir))
-(defn positions [dirs] (reductions apply-dir [0 0 0] dirs))
+
+(defn positions [dirs] (reductions #(mapv + %1 %2) [0 0 0] dirs))
 
 ;; -----------------------------------------------------------------------------
 
-(def input (str/split ((comp str/trim slurp io/resource) "2017/11/input.txt") #","))
+(def input
+  ((comp #(map (comp move-dir keyword) %) #(str/split % #",") str/trim slurp io/resource)
+   "2017/11/input.txt"))
 
 (comment
 
   ;; Part One
 
-  (manhattan (apply-dirs (map move-dir input)))
+  (manhattan (apply mapv + input))
   ;; => 720
 
 
   ;; Part Two
 
-  (apply max (map manhattan (positions (map move-dir input))))
+  (apply max (map manhattan (positions input)))
   ;; => 1485
 
   )
